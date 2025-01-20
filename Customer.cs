6fe444e3215d5a9bd8057b1e5573df2f6ad6ed9d
debeski <debeski1@gmail.com>
2025-01-20 16:57:29 +0800

    initial

---
+++ Customer.cs
@@ -0,0 +1,7 @@
+public class Customer
+{
+    public int id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double ODLimit { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Customers App");
+List<Customer> customers=new List<Customer>
+{
+    new Customer{id=1,FirstName="John",LastName="Doe",ODLimit=98000.50},
+    new Customer{id=2,FirstName="Abubaker",LastName="Naji",ODLimit=108000.50},
+    new Customer{id=3,FirstName="Abdo",LastName="Farhat",ODLimit=125000.50},
+    new Customer{id=4,FirstName="Ahmed",LastName="DeBeski",ODLimit=308000.50},
+    new Customer{id=5,FirstName="Mr.",LastName="Mohamed",ODLimit=4508000.50},
+};
+foreach(var c in customers)
+{
+    Console.WriteLine("ID: \t"+c.id +"\t Full Name: \t" + c.FirstName +" "+c.LastName +"\t OverDraftLimit\t"+c.ODLimit);
+}
