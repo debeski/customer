257ff922b37b93d5e4366c7f76d681bd9e7dbc4f
debeski <debeski1@gmail.com>
2025-01-20 17:05:00 +0800

    initial2

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Welcome to Customers App");
 List<Customer> customers=new List<Customer>
 {
     new Customer{id=1,FirstName="John",LastName="Doe",ODLimit=98000.50},
-    new Customer{id=2,FirstName="Abubaker",LastName="Naji",ODLimit=108000.50},
+    new Customer{id=2,FirstName="bubaker",LastName="Naji",ODLimit=108000.50},
     new Customer{id=3,FirstName="Abdo",LastName="Farhat",ODLimit=125000.50},
     new Customer{id=4,FirstName="Ahmed",LastName="DeBeski",ODLimit=308000.50},
     new Customer{id=5,FirstName="Mr.",LastName="Mohamed",ODLimit=4508000.50},
